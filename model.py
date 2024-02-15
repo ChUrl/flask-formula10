ec825ca5afd1a72f04ae8a42477523b0cb0950a8
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-02-15 23:09:27 +0100

    Improve model by storing objects serialized

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey, PickleType
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
+import json
 
 db = SQLAlchemy()
 
@@ -133,172 +136,95 @@ class RaceGuess(db.Model):
 
 class TeamWinners(db.Model):
     __tablename__ = "teamwinners"
-    __csv_header__ = ["id", "user_id", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    __csv_header__ = ["id", "user_id", "winner_ids_json"]
 
     def from_csv(self, row):
         self.id = int(row[0])
-        self.user_id = str(row[1])
-        self.a_id = str(row[2])
-        self.b_id = str(row[3])
-        self.c_id = str(row[4])
-        self.d_id = str(row[5])
-        self.e_id = str(row[6])
-        self.f_id = str(row[7])
-        self.g_id = str(row[8])
-        self.h_id = str(row[9])
-        self.i_id = str(row[10])
-        self.j_id = str(row[11])
+        self.user_id = int(row[1])
+        self.winner_ids_json = str(row[2])
         return self
 
     def to_csv(self):
         return [
             self.id,
             self.user_id,
-            self.a_id,
-            self.b_id,
-            self.c_id,
-            self.d_id,
-            self.e_id,
-            self.f_id,
-            self.g_id,
-            self.h_id,
-            self.i_id,
-            self.j_id
+            self.winner_ids_json
         ]
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[str] = mapped_column(ForeignKey("user.name"))
-    a_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    b_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    c_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    d_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    e_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    f_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    g_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    h_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    i_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    j_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
+    winner_ids_json: Mapped[str] = mapped_column(String(512))
+
+    @property
+    def winner_ids(self) -> List[str]:
+        return json.loads(self.winner_ids_json)
+
+    @winner_ids.setter
+    def winner_ids(self, new_winner_ids: List[str]):
+        self.winner_ids_json = json.dumps(new_winner_ids)
 
     # Relationships
     user: Mapped["User"] = relationship("User", foreign_keys=[user_id])
-    a: Mapped["Driver"] = relationship("Driver", foreign_keys=[a_id])
-    b: Mapped["Driver"] = relationship("Driver", foreign_keys=[b_id])
-    c: Mapped["Driver"] = relationship("Driver", foreign_keys=[c_id])
-    d: Mapped["Driver"] = relationship("Driver", foreign_keys=[d_id])
-    e: Mapped["Driver"] = relationship("Driver", foreign_keys=[e_id])
-    f: Mapped["Driver"] = relationship("Driver", foreign_keys=[f_id])
-    g: Mapped["Driver"] = relationship("Driver", foreign_keys=[g_id])
-    h: Mapped["Driver"] = relationship("Driver", foreign_keys=[h_id])
-    i: Mapped["Driver"] = relationship("Driver", foreign_keys=[i_id])
-    j: Mapped["Driver"] = relationship("Driver", foreign_keys=[j_id])
+    _winners = None
+
+    @property
+    def winners(self) -> List[Driver]:
+        if self._winners is None:
+            self._winners = [
+                driver for driver in Driver.query.all() if driver.name in self.winner_ids
+            ]
+
+        return self._winners
 
 
 class PodiumDrivers(db.Model):
     __tablename__ = "podiumdrivers"
-    __csv_header__ = ["id", "user_id", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]
+    __csv_header__ = ["id", "user_id", "podium_ids_json"]
 
     def from_csv(self, row):
         self.id = int(row[0])
-        self.user_id = str(row[1])
-        self.a_id = str(row[2])
-        self.b_id = str(row[3])
-        self.c_id = str(row[4])
-        self.d_id = str(row[5])
-        self.e_id = str(row[6])
-        self.f_id = str(row[7])
-        self.g_id = str(row[8])
-        self.h_id = str(row[9])
-        self.i_id = str(row[10])
-        self.j_id = str(row[11])
-        self.k_id = str(row[12])
-        self.l_id = str(row[13])
-        self.m_id = str(row[14])
-        self.n_id = str(row[15])
-        self.o_id = str(row[16])
-        self.p_id = str(row[17])
-        self.q_id = str(row[18])
-        self.r_id = str(row[19])
-        self.s_id = str(row[20])
-        self.t_id = str(row[21])
+        self.user_id = int(row[1])
+        self.podium_ids_json = str(row[2])
         return self
 
     def to_csv(self):
         return [
             self.id,
             self.user_id,
-            self.a_id,
-            self.b_id,
-            self.c_id,
-            self.d_id,
-            self.e_id,
-            self.f_id,
-            self.g_id,
-            self.h_id,
-            self.i_id,
-            self.j_id,
-            self.k_id,
-            self.l_id,
-            self.m_id,
-            self.n_id,
-            self.o_id,
-            self.p_id,
-            self.q_id,
-            self.r_id,
-            self.s_id,
-            self.t_id
+            self.podium_ids_json
         ]
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     user_id: Mapped[str] = mapped_column(ForeignKey("user.name"))
-    a_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    b_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    c_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    d_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    e_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    f_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    g_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    h_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    i_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    j_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    k_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    l_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    m_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    n_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    o_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    p_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    q_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    r_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    s_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
-    t_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
+    podium_ids_json: Mapped[str] = mapped_column(String(512))
+
+    @property
+    def podium_ids(self) -> List[str]:
+        return json.loads(self.podium_ids_json)
+
+    @podium_ids.setter
+    def podium_ids(self, new_podium_ids: List[str]):
+        self.podium_ids_json = json.dumps(new_podium_ids)
 
     # Relationships
     user: Mapped["User"] = relationship("User", foreign_keys=[user_id])
-    a: Mapped["Driver"] = relationship("Driver", foreign_keys=[a_id])
-    b: Mapped["Driver"] = relationship("Driver", foreign_keys=[b_id])
-    c: Mapped["Driver"] = relationship("Driver", foreign_keys=[c_id])
-    d: Mapped["Driver"] = relationship("Driver", foreign_keys=[d_id])
-    e: Mapped["Driver"] = relationship("Driver", foreign_keys=[e_id])
-    f: Mapped["Driver"] = relationship("Driver", foreign_keys=[f_id])
-    g: Mapped["Driver"] = relationship("Driver", foreign_keys=[g_id])
-    h: Mapped["Driver"] = relationship("Driver", foreign_keys=[h_id])
-    i: Mapped["Driver"] = relationship("Driver", foreign_keys=[i_id])
-    j: Mapped["Driver"] = relationship("Driver", foreign_keys=[j_id])
-    k: Mapped["Driver"] = relationship("Driver", foreign_keys=[k_id])
-    l: Mapped["Driver"] = relationship("Driver", foreign_keys=[l_id])
-    m: Mapped["Driver"] = relationship("Driver", foreign_keys=[m_id])
-    n: Mapped["Driver"] = relationship("Driver", foreign_keys=[n_id])
-    o: Mapped["Driver"] = relationship("Driver", foreign_keys=[o_id])
-    p: Mapped["Driver"] = relationship("Driver", foreign_keys=[p_id])
-    q: Mapped["Driver"] = relationship("Driver", foreign_keys=[q_id])
-    r: Mapped["Driver"] = relationship("Driver", foreign_keys=[r_id])
-    s: Mapped["Driver"] = relationship("Driver", foreign_keys=[s_id])
-    t: Mapped["Driver"] = relationship("Driver", foreign_keys=[t_id])
+    _podiums = None
+
+    @property
+    def podiums(self) -> List[Driver]:
+        if self._podiums is None:
+            self._podiums = [
+                driver for driver in Driver.query.all() if driver.name in self.podium_ids
+            ]
+
+        return self._podiums
 
 
 class SeasonGuess(db.Model):
     __tablename__ = "seasonguess"
     __csv_header__ = ["id", "user_id",
-                      "hot_take", "p2_constructor", "most_overtakes", "most_dnfs", "team_winners", "podium_drivers"]
+                      "hot_take", "p2_constructor_id", "most_overtakes_id", "most_dnfs_id", "most_gained_id",
+                      "most_lost_id", "team_winners_id", "podium_drivers_id"]
 
     def from_csv(self, row):
         self.id = int(row[0])
@@ -307,8 +233,10 @@ class SeasonGuess(db.Model):
         self.p2_constructor_id = str(row[3])
         self.most_overtakes_id = str(row[4])
         self.most_dnfs_id = str(row[5])
-        self.team_winners_id = int(row[6])
-        self.podium_drivers_id = int(row[6])
+        self.most_gained_id = str(row[6])
+        self.most_lost_id = str(row[6])
+        self.team_winners_id = int(row[8])
+        self.podium_drivers_id = int(row[9])
         return self
 
     def to_csv(self):
@@ -319,6 +247,8 @@ class SeasonGuess(db.Model):
             self.p2_constructor_id,
             self.most_overtakes_id,
             self.most_dnfs_id,
+            self.most_gained_id,
+            self.most_lost_id,
             self.team_winners_id,
             self.podium_drivers_id
         ]
@@ -329,6 +259,8 @@ class SeasonGuess(db.Model):
     p2_constructor_id: Mapped[str] = mapped_column(ForeignKey("team.name"))
     most_overtakes_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
     most_dnfs_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
+    most_gained_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
+    most_lost_id: Mapped[str] = mapped_column(ForeignKey("driver.name"))
     team_winners_id: Mapped[int] = mapped_column(ForeignKey("teamwinners.id"))
     podium_drivers_id: Mapped[int] = mapped_column(ForeignKey("podiumdrivers.id"))
 
@@ -337,5 +269,7 @@ class SeasonGuess(db.Model):
     p2_constructor: Mapped["Team"] = relationship("Team", foreign_keys=[p2_constructor_id])
     most_overtakes: Mapped["Driver"] = relationship("Driver", foreign_keys=[most_overtakes_id])
     most_dnfs: Mapped["Driver"] = relationship("Driver", foreign_keys=[most_dnfs_id])
+    most_gained: Mapped["Driver"] = relationship("Driver", foreign_keys=[most_gained_id])
+    most_lost: Mapped["Driver"] = relationship("Driver", foreign_keys=[most_lost_id])
     team_winners: Mapped["TeamWinners"] = relationship("TeamWinners", foreign_keys=[team_winners_id])
     podium_drivers: Mapped["PodiumDrivers"] = relationship("PodiumDrivers", foreign_keys=[podium_drivers_id])
