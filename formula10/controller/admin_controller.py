457a80e58e2d57bd1032b01d4279d6bfc9a22d4b
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-02-26 22:35:22 +0100

    Disable load dynamic admin endpoints

--- formula10/controller/admin_controller.py
+++ formula10/controller/admin_controller.py
@@ -4,7 +4,7 @@ from flask import redirect, render_template, request
 from werkzeug import Response
 
 from formula10.database.update_queries import update_race_result, update_user
-from formula10.database.import_export import export_dynamic_data, reload_dynamic_data, reload_static_data
+from formula10.database.import_export import export_dynamic_data, reload_static_data
 from formula10.frontend.template_model import TemplateModel
 from formula10 import app
 
@@ -15,11 +15,11 @@ def save() -> Response:
     return redirect("/")
 
 
-@app.route("/load/all")
-def load() -> Response:
-    reload_static_data()
-    reload_dynamic_data()
-    return redirect("/")
+# @app.route("/load/all")
+# def load() -> Response:
+#     reload_static_data()
+#     reload_dynamic_data()
+#     return redirect("/")
 
 
 @app.route("/load/static")
@@ -28,10 +28,10 @@ def load_static() -> Response:
     return redirect("/")
 
 
-@app.route("/load/dynamic")
-def load_dynamic() -> Response:
-    reload_dynamic_data()
-    return redirect("/")
+# @app.route("/load/dynamic")
+# def load_dynamic() -> Response:
+#     reload_dynamic_data()
+#     return redirect("/")
 
 
 @app.route("/result")
