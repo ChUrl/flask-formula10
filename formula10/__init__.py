1e4c9a1ad676c5c39a7e6fc259fadb3a01056646
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-02-26 23:06:34 +0100

    Add race countdown

--- formula10/__init__.py
+++ formula10/__init__.py
@@ -4,8 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 app: Flask = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///formula10.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Session cookie is used to propagate message to error page
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'ich stinke nach maggi'
+
 app.url_map.strict_slashes = False
 
 db: SQLAlchemy = SQLAlchemy()
@@ -22,17 +25,9 @@ import formula10.controller.error_controller
 # TODO
 # General
 
-# Date stuff:
-# - Date lock race guess entering + race result entering + season guess entering (frontend + backend)
-# - Some info popup that can be populated
 # - Show timer until next race?
 
-# - Choose "place to guess" late before the race? Make a page for this?
-# - Store currently active user inside cookie
-
-# - Make user order changeable using drag'n'drop?
 # - Show place when entering race result (would require updating the drag'n'drop code...)
-# - Show cards of previous race results, like with season guesses?
 # - Don't write full 2024 date, just 24 or leave out completely, to make column smaller
 
 # Statistics
@@ -41,3 +36,7 @@ import formula10.controller.error_controller
 # - Order user table by points + display points somewhere
 # - Highlight currently correct values for some season guesses (e.g. current most dnfs)
 # - Generate static diagram using chart.js + templating the js (funny yikes)
+
+# Possible but probably not
+# - Show cards of previous race results, like with season guesses?
+# - Make user order changeable using drag'n'drop?
---
+++ formula10/static/script/countdown.js
@@ -0,0 +1,31 @@
+// Set the date we're counting down to
+var raceTime = document.getElementById("race_date").innerHTML
+console.log(raceTime)
+var countDownDate = new Date(raceTime).getTime();
+console.log(countDownDate)
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("race_date_countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the countdown is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("race_date_countdown").innerHTML = "GO GO GO";
+  }
+
+}, 1000);
